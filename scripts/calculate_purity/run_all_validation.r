942a03aeef5c65240a984cf46a4b77db7867dddf
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-05-08 10:35:26 +0200

    Improve documentation of run_all_validation.r

--- scripts/calculate_purity/run_all_validation.r
+++ scripts/calculate_purity/run_all_validation.r
@@ -4,39 +4,41 @@
 # INSTALLING AND/OR LOADING PACKAGES
 # ==================================
 
+#Install and load optparse to parse command line arguments
 if(!requireNamespace("optparse", quietly = TRUE)) {
   install.packages("optparse", quietly = TRUE) }
 
 suppressPackageStartupMessages(library(optparse))
 
-if(!requireNamespace("caTools", quietly = TRUE)) {
-  install.packages("caTools", quietly = TRUE) }
-
-suppressPackageStartupMessages(library(caTools))
-
+#Install and load parallel to run the script in parallel
 if(!requireNamespace("parallel", quietly = TRUE)) {
   install.packages("parallel", quietly = TRUE) }
 
 suppressPackageStartupMessages(library(parallel))
 
+#Install and load doSnow to run the script in parallel
 if(!requireNamespace("doSNOW", quietly = TRUE)) {
   install.packages("doSNOW", quietly = TRUE) }
 
 suppressPackageStartupMessages(library(doSNOW))
 
+#Install and load foreach to run the script in parallel
 if(!requireNamespace("foreach", quietly = TRUE)) {
   install.packages("foreach", quietly = TRUE) }
 
 suppressPackageStartupMessages(library(foreach))
 
+#Install and load Kendall to create a progress bar
 if(!requireNamespace("Kendall", quietly = TRUE)) {
   install.packages("Kendall", quietly = TRUE) }
 
 suppressPackageStartupMessages(library(Kendall))
+
 # ==================================
 # CONFIGURING COMMAND LINE ARGUMENTS
 # ==================================
 
+#Defining command line arguments
 argument_list <- list(
 
   make_option(c("-c", "--cores"), type="integer", default=1,  
@@ -77,6 +79,7 @@ argument_list <- list(
 
 )
 
+#Parsing arguments
 arguments <- parse_args(OptionParser(option_list=argument_list, 
                                     description="This program estimates the Purity values of samples based on the beta values of each sample's CpGs."))
 
@@ -85,14 +88,16 @@ arguments <- parse_args(OptionParser(option_list=argument_list,
 # LOADING THE REQUIRED FUNCTIONS
 # ==============================
 
+# Getting the directory of the functions sourced on this script 
+# (They must be located on the same directory than the main script)
 dir <- commandArgs()[4]
 dir <- gsub("--file=", "", dir)
 
-
+#Getting the complete path of the functions to be called
 fun1 <- gsub("run_all_validation.r", "predicting_purity.r", dir)
 fun2 <- gsub("run_all_validation.r", "purity_coverage.r", dir)
 
-#Sourcing the functions to be used
+#Sourcing the functions
 source(fun1)
 source(fun2)
 
@@ -118,31 +123,41 @@ list_of_predicted_intervals <- list()
 # CONFIGURING PARALLELIZATION
 # ===========================
 
+# Printing the number of cores to be used
 cat("\nUsing", arguments$cores, "cores\n")
 
+# Creatinhg clusters to run the script in  parallel
 cl <- makeCluster(arguments$cores)
+
+#Registering the clusters
 registerDoSNOW(cl)
 
 # ========================================
 # RUNNING THE ANALYSIS WITH A PROGRESS BAR
 # ========================================
 
+# Printing command line message
 cat("\nRunning the analysis...\n\n")
 
-
+# Getting the names of the samples to analyse
 samples <- colnames(unadj_validation)
 
+# Defining the progress bar
 p_bar <- txtProgressBar(min = 0, 
                         max = length(samples), 
                         style = 3)
 
+# Creating a function to follow the execution of the script
 progress <- function(n) setTxtProgressBar(p_bar, n)
-
 opts <- list(progress = progress)
 
+# Running the sourced functions in parallel for each sample. The execution level will be followed through a progress bar
 list_of_predicted_intervals <- foreach(s = samples, .packages = "Kendall", .options.snow = opts) %dopar% {
 
+  # Defining an empty dataframe to add the all the 1-Purity predicted intervals for all the CpGs of a sample
   interval_df <- data.frame()
+
+  # Predicting all the 1-Purity intervals for each CpG of each sample and append them to the empty interval_df
   for (cpg in rownames(unadj_validation)) {
     interval_df <- rbind(interval_df, predicting_purity(beta=unadj_validation[cpg, s],
                                                         slopes=my_slopes[cpg, ],
@@ -155,19 +170,23 @@ list_of_predicted_intervals <- foreach(s = samples, .packages = "Kendall", .opti
                                                         ))
   }
 
-  # Return a named list directly
+  # Calculate the 1-Purity estimate and interval for the sample analysed.
+  # The results with be shown in list named with the sample id
   list(name = s, value = purity_value_per_sample(pred_purity_confidence=interval_df,
                                                  interval_threshold=arguments$percentage_to_interval))
 }
 
+# Stop the defined clusters
 stopCluster(cl)
 
-
+# Append the list defined for each sample to the list containing the predicted values for all the samples.
+# The sample id is used to identify each element of the list
 list_of_predicted_intervals <- setNames(lapply(list_of_predicted_intervals, function(x) x$value), sapply(list_of_predicted_intervals, function(x) x$name))
 
-
+# Save the list of predicted values as an R object
 saveRDS(list_of_predicted_intervals, file=paste(arguments$output_location, arguments$output_filename, ".RData", sep=""))
 
+#Print message to show the end of the execution
 cat("\n\n**********************\n")
 cat("   PROCESS FINISHED\n")
 cat("**********************\n")
