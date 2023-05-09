18ab479cbfd6e63fae0d5451c38ac5d6260ae4f3
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-05-09 12:49:45 +0200

    Updating script to allow to divide the CpGs in function of the infinium method

--- scripts/get_data_to_analyse/preprocessing_data.r
+++ scripts/get_data_to_analyse/preprocessing_data.r
@@ -80,7 +80,15 @@ argument_list <- list(
 
   make_option(c("-c", "--column_chromosomes"), type="character",
               help="Select the name of column of the dataframe containig the information about the chromosomes.",
-              metavar="[column name]")            
+              metavar="[column name]"),
+
+  make_option(c("-d", "--split_based_on_design"), type="logical", default=FALSE,
+              help="If the user want to split the data based on the beta value detremination methods (infinium I or II) this argument should be set to TRUE)",
+              metavar="[TRUE/FALSE]"),
+
+  make_option(c("-D", "--path_to_design_list"), type="character",
+              help="The path to the r object containing a list with the CpGs quantified with each infinium method.",
+              metavar="[path]")          
 
 )
 
@@ -137,6 +145,19 @@ if (arguments$filter_non_autosomes) {
 
 }
 
+# ===================================================
+# DIVIDING DATASET IN FUNCTION OF THE INFINIUM METHOD
+# ===================================================
+
+if (arguments$split_based_on_design) {
+
+  designs <- readRDS(arguments$path_to_design_list)
+
+  betas_I <- betas[rownames(betas) %in% designs[["I"]]]
+  betas_II <- betas[rownames(betas) %in% designs[["II"]]]
+
+}
+
 # ========================================
 # CREATING TRAINING AND VALIDATION SUBSETS
 # ========================================
@@ -144,23 +165,46 @@ if (arguments$filter_non_autosomes) {
 #If the user has selected to filter sexual chromosomes the following code will be run
 if (arguments$split_in_training_and_validation) {
 
-cat("\n\nCreating training and validation datasets...\n")
+  if (!arguments$split_based_on_design) {
+    cat("\n\nCreating training and validation datasets...\n")
 
-#Setting the seed for spliting the data
-set.seed(1)
+    #Setting the seed for spliting the data
+    set.seed(1)
 
-#Splitting the data
-split_ratio <- 1 - arguments$percentage_to_validation/100
-train_samples <- sample.split(colnames(betas), SplitRatio=split_ratio)
+    #Splitting the data
+    split_ratio <- 1 - arguments$percentage_to_validation/100
+    train_samples <- sample.split(colnames(betas), SplitRatio=split_ratio)
 
-#Creating training and validation datasets for the beta values
-unadj_training <- betas[,train_samples]
-unadj_validation <- betas[,!train_samples]
+    #Creating training and validation datasets for the beta values
+    unadj_training <- betas[,train_samples]
+    unadj_validation <- betas[,!train_samples]
 
-#Creating training and validation datasets for the purity_vector
-purity_training <- purities[train_samples]
-purity_validation <- purities[!train_samples]
+    #Creating training and validation datasets for the purity_vector
+    purity_training <- purities[train_samples]
+    purity_validation <- purities[!train_samples]
+  } else {
 
+    cat("\n\nCreating training and validation datasets for each of the beta estimation method...\n")
+
+    #Setting the seed for spliting the data
+    set.seed(1)
+
+    #Splitting the data
+    split_ratio <- 1 - arguments$percentage_to_validation/100
+    train_samples <- sample.split(colnames(betas_I), SplitRatio=split_ratio)
+    
+
+    #Creating training and validation datasets for the beta values
+    unadj_training_I <- betas_I[,train_samples]
+    unadj_training_II <- betas_II[,train_samples]
+    unadj_validation_I <- betas_I[,!train_samples]
+    unadj_validation_II <- betas_II[,!train_samples]
+
+    #Creating training and validation datasets for the purity_vector
+    purity_training <- purities[train_samples]
+    purity_validation <- purities[!train_samples]
+
+  }
 }
 
 # =======================
@@ -172,6 +216,8 @@ cat("\n\nSaving the data...\n")
 #Run if the user has selected to split the data in training and validation subsets
 if (arguments$split_in_training_and_validation) {
 
+   if (!arguments$split_based_on_design) {  
+
     #Saving beta values
     saveRDS(unadj_training, paste(arguments$output_directory, "/betas_training.RData", sep=""))
     saveRDS(unadj_validation, paste(arguments$output_directory, "/betas_validation.RData", sep=""))
@@ -180,14 +226,41 @@ if (arguments$split_in_training_and_validation) {
     saveRDS(purity_training, paste(arguments$output_directory, "/purity_training.RData", sep=""))
     saveRDS(purity_validation, paste(arguments$output_directory, "/purity_validation.RData", sep=""))
 
+   } else {
+
+    #Saving beta values
+    saveRDS(unadj_training_I, paste(arguments$output_directory, "/betas_training_I.RData", sep=""))
+    saveRDS(unadj_training_II, paste(arguments$output_directory, "/betas_training_II.RData", sep=""))
+    saveRDS(unadj_validation_I, paste(arguments$output_directory, "/betas_validation_I.RData", sep=""))
+    saveRDS(unadj_validation_II, paste(arguments$output_directory, "/betas_validation_II.RData", sep=""))
+
+    #Saving purity value
+    saveRDS(purity_training, paste(arguments$output_directory, "/purity_training.RData", sep=""))
+    saveRDS(purity_validation, paste(arguments$output_directory, "/purity_validation.RData", sep=""))
+
+   }
+
 #Run if the user has not selected to split the data in training and validation subsets
 } else {
+
+  if (!arguments$split_based_on_design) {  
 
     #Saving beta values
     saveRDS(arguments$betas, paste(arguments$output_directory, "/betas.RData", sep=""))
     
     #Saving purity values
     saveRDS(arguments$purity, paste(arguments$output_directory, "/purity.RData", sep=""))
+
+  } else {
+
+    #Saving beta values
+    saveRDS(arguments$betas_I, paste(arguments$output_directory, "/betas_I.RData", sep=""))
+    saveRDS(arguments$betas_I, paste(arguments$output_directory, "/betas_II.RData", sep=""))
+    
+    #Saving purity values
+    saveRDS(arguments$purity, paste(arguments$output_directory, "/purity.RData", sep=""))
+
+  }
 
 }
 
