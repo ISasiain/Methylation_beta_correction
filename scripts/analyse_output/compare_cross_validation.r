f0cd0a7703bdbe2a5eb59ec0b93568f87d692e54
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-08-31 14:20:21 +0200

    Fixing errors and improving the code

--- scripts/analyse_output/compare_cross_validation.r
+++ scripts/analyse_output/compare_cross_validation.r
@@ -75,6 +75,13 @@ vec_of_folds <- strsplit(arguments$names_of_the_folds, split=",")[[1]]
 # Splitting the provided argument in a vector of predictions to analyse
 vec_of_preds <- strsplit(arguments$names_of_the_predictions_to_compare, split=",")[[1]]
 
+# Apply strsplit to each element of the Prediction column
+to_order <- as.numeric(sapply(vec_of_preds, function(x) strsplit(x, split = "_")[[1]][2]))
+
+# Sorting the vector
+vec_of_preds <- vec_of_preds[order(to_order)]
+
+
 # Creating an empty list for the predictied values per prediction and fold
 
 # Creating a list containing each prediction made as an element
@@ -99,50 +106,33 @@ for (prediction in names(prediction_list)) {
 # Loading the actual 1-Purity vector into a variable
 actual_1_minus_P <- readRDS(arguments$reference_purity)
 
-#Adding this to adapt the sample names
-names(actual_1_minus_P) <- paste(names(actual_1_minus_P), "-01A", sep="")
+# Create an empty list to store predictions
+prediction_list <- vector("list", length(vec_of_preds))
 
+# Iterate through predictions and folds and append the predictions
+for (pred in vec_of_preds) {
+  for (fold in vec_of_folds) {
 
-# ESTIMATED PURITIES
+    # Getting the path to each prediction
+    path_to_prediction <- paste(arguments$directory, "/", pred, "/", fold, sep="")
+    pattern <- paste("*_", gsub("_", "", pred), "\\.RData", sep="")
 
-# Get all the combinations of predictions and folds
-combinations <- expand.grid(vec_of_preds, vec_of_folds)
-
-# Creating a function to append each prediction to the list
-
-append_prediction <- function(prediction, fold, directory) {
-
-    #Getting the path to each prediction (SLIGHTLY HARDCODED!!!  WE ARE ASSUMING THAT THE FILE ENDS WITH cpgXXX.RData)
-    path_to_prediction <- paste(directory, "/", prediction, "/", fold, sep="")
-    pattern <- paste("*_", gsub("_", "", prediction), "\\.RData", sep="")
 
     # List files in the directory that match the pattern
     matching_files <- list.files(path_to_prediction, pattern = pattern, full.names = TRUE)
 
-    #PROBLEM!!! SOME FILES HAVE NOT BEEN GENERATED!!!!!!!1
-    print(prediction)
-    # Print the matching files
-    print(matching_files)
-
+    # Raise an error if more than one paths are identified
     if (length(matching_files) != 1) {
-
       stop("More than one file containing the estimated purity per prediction and fold has been detected. Execution halted.")
-
     }
 
-    #Appending each prediction
-    prediction_list[[prediction]][[fold]] <- readRDS(matching_files[1])
+    #Append to list
+    prediction_list[[pred]][[fold]] <- readRDS(matching_files[1])
+
+  }
 }
 
-#Applying the function to each row of the combinations dataframe using lapply
 
-lapply(X=seq_len(nrow(combinations)), 
-       FUN=function(i) {append_prediction(prediction=combinations$Var1[i],
-                                          fold=combinations$Var2[i], 
-                                          directory=arguments$directory)
-                        }
-)
-                
 # =====================================================================
 #          DETERMINING QUALITY METRICS (distance to estimate)
 # =====================================================================
@@ -151,25 +141,32 @@ lapply(X=seq_len(nrow(combinations)),
 dti_df <- data.frame(matrix(nrow = length(vec_of_preds), ncol = length(vec_of_folds)))
 
 # Adding column names and row names
-colnames(dti_df) <- vec_of_folds
 rownames(dti_df) <- vec_of_preds
+colnames(dti_df) <- vec_of_folds
 
-for (prediction in vec_of_preds) {
+print(vec_of_preds)
+print(vec_of_folds)
+
+# Iterate through the predictions and folds to determine the dist_to_est (quality metric) per each
+for (pred in vec_of_preds) {
 
   for (fold in vec_of_folds) {
 
-    dti_df[prediction, fold] <- mean(
+    dti_df[pred, fold] <- mean(
       sapply(
-        names(prediction_ls[[prediction]][[fold]]),
+        names(prediction_list[[pred]][[fold]]),
         FUN = function (sample) {
+        
+
         # Determining the distance to the estimate (the mean is calculated in case there were more than one maximums. THis can be removed after softening the coverage plot)
-        dis_to_estimate <- mean(abs(1-actual_1_minus_P[sample] - prediction_ls[[prediction]][[fold]][[sample]][["1-Pur_estimates"]]),4)
+        mean(abs(1 - actual_1_minus_P[sample] - prediction_list[[pred]][[fold]][[sample]][["1-Pur_estimates"]]),4)
         }
       )
     )
   }
 }
 
+print(dti_df)
 
 # ================================================
 #                PLOTTING RESULTS
@@ -179,17 +176,17 @@ for (prediction in vec_of_preds) {
 # Transforming the row names into a column (using tibble)
 dti_df <- dti_df %>% rownames_to_column("Prediction")
 
-
 # Transforming the dataframe to long format to be plotted
-dti_df_long <- pivot_longer(dti_df, cols="Prediction", names_to="Fold", values_to="Distance_to_estimate")
+dti_df <- dti_df %>% pivot_longer(cols=all_of(vec_of_folds), 
+                                  names_to="Fold", 
+                                  values_to="Distance_to_estimate")
 
 # Plotting the results 
-
-ggplot(dti_df_long, aes(x = Prediction, y = Quality, color = Fold)) +
-  geom_boxplot() +
-  geom_point(position = position_jitterdodge(jitter.width = 0.2, dodge.width = 0.7)) +
-  labs(x = "Prediction", y = "Quality Metric") +
-  theme_classic()
+ggplot(dti_df, aes(x = factor(Prediction, levels = vec_of_preds), y = Distance_to_estimate)) +
+  geom_boxplot(fill="olivedrab4") +  # Add a boxplot for each prediction
+  labs(x = "Prediction", y = "Mean distance to estimate (% points)") +
+  theme_classic() + 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1))
 
 # Saving the plot
 ggsave(paste(arguments$output_prefix, "cross_validation.boxplot.png", sep="."))
