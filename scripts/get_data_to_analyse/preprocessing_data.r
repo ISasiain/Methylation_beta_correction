31760726406b6a7a53a106309b5cdfe07ae5d54b
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-09-19 16:44:04 +0200

    Updating script, Removing unnecesary steps

--- scripts/get_data_to_analyse/preprocessing_data.r
+++ scripts/get_data_to_analyse/preprocessing_data.r
@@ -60,30 +60,16 @@
 #
 #    The number of output files will depend on the options chosen;
 #
-#     * If the user has not selected to split the data on training and validation datasets and in function 
-#       of the infinium design;
+#     * If the user has not selected to split the data on training and validation datasets;
 #
 #       -> An R object file containing a dataframe with the betas of each sample and cpg.
 #       -> An R object file containing a named vector with the actual purity values of each sample.
 #
-#     * If the user has selected to split the data on training and validation datasets but not in function 
-#       of the infinium design;
+#     * If the user has selected to split the data on training and validation datasets;
 #
 #       -> Two R object files, training and validation, containing dataframe with the betas of ecah sample and cpg. 
 #       -> Two R object files, training and validation, containing named vectors with the actual purity values of each sample.
 #
-#     * If the user has selected not to split the data on training and validation datasets but splitting it in function 
-#       of the infinium design;
-#
-#       -> Two R object files, with cpgs determined using infiniumI and infiniumII, containing dataframe with the betas of ecah sample and cpg. 
-#       -> An R object file containing a named vector with the actual purity values of each sample.
-#
-#     * If the user has selected to split the data on training and validation datasets qnd also in function 
-#       of the infinium design;
-#
-#       -> Four R object files, training and validation with cpgs determined using infiniumI and infiniumII, containing dataframe with the betas
-#          of ecah sample and cpg. 
-#       -> Two R object files, training and validation, containing named vectors with the actual purity values of each sample.
 #
 ## - USAGE:
 #
@@ -92,12 +78,12 @@
 #     """
 #     Rscript path_to_script/preprocessing_data.r -s [TRUE/FALSE] -B [path_to_betas] -P [path_to_purities] -b [betas_name] -p [purities_name]
 #     -S [TRUE/FALSE] -v [float/int] -o [path_to_output] -f [TRUE/FALSE] -a [path_to_annotation_file] -a [name_of_annotation_df] -c [chr_column_name]
-#     -d [TRUE/FALSE] -D [path_to_design_file] -N [TRUE/FALSE] -n [integer]
+#     -N [TRUE/FALSE] -n [integer]
 #     """
 #     
 #     *The function of the command line options are the following; 
 #
-#       -a: The user must especify if the purity and betas are in the same R object.
+#       -s: The user must especify if the purity and betas are in the same R object.
 #       -B: The path to the R object with the data to process must be entered here. If the purities and betas are in different R object files the one containing the betas should be entered here
 #       -P: The path to the R object with the purities must be entered here. This should only be used if the purities and betas are in different R objects.
 #       -b: The name of the object that contains the beta values inside the R object must be entered here
@@ -109,14 +95,12 @@
 #       -A: Specify the name of the path containing the annotation file.
 #       -a: Specify the name of the dataframe containing the annotation information.
 #       -c: Select the name of column of the dataframe containig the information about the chromosomes.
-#       -d: If the user wants to split the data based on the infinium design (infinium I or II) this argument should be set to TRUE
-#       -D: he path to the r object containing a list with the CpGs quantified with each infinium method.
 #       -N: This argument should be sent to true if the user wants to get only the betas of the CpGs with the highest variance
 #       -n: The number of the most variant CpGs to be included in the output should be included here
 #
 ## - VERSION: 1.0
 #
-## - DATE: 18/05/2023
+## - DATE: 23/05/2023
 #
 ## - AUTHOR: Iñaki Sasiain Casado
 
@@ -195,14 +179,6 @@ argument_list <- list(
               help="Select the name of column of the dataframe containig the information about the chromosomes.",
               metavar="[column name]"),
 
-  make_option(c("-d", "--split_based_on_design"), type="logical", default=FALSE,
-              help="If the user wants to split the data based on the beta value detremination methods (infinium I or II) this argument should be set to TRUE",
-              metavar="[TRUE/FALSE]"),
-
-  make_option(c("-D", "--path_to_design_list"), type="character",
-              help="The path to the r object containing a list with the CpGs quantified with each infinium method.",
-              metavar="[path]"),
-
   make_option(c("-N", "--get_only_CpGs_with_highest_variance"), type="logical", default=FALSE,
               help="This argument should be sent to true if the user wants to get only the betas of the CpGs with the highest variance",
               metavar="[TRUE/FALSE]"),
@@ -219,7 +195,7 @@ arguments <- parse_args(OptionParser(option_list=argument_list,
 
 
 # ===========
-# LODING DATA
+# LOADING DATA
 # ===========
 
 cat("\n\nLoading the data...\n")
@@ -265,91 +241,55 @@ if (arguments$filter_non_autosomes) {
 
 }
 
-
-# =================================
-#    GETTING MOST VARIABLE CPGs
-# =================================
-
-# If the user has selected to get only the most variable CpGs the following code will be run
-if(arguments$get_only_CpGs_with_highest_variance) {
-
-  # Detemine the variance of all the rows (CpGs)
-  cpgs_variance <- apply(
-                        betas,
-                        MARGIN=1,
-                        FUN=var
-                   )
-
-  # Create a vector to sort the rows. Get only the number of rows containing the CpGs that want to be included
-  sorting_vec <- order(cpgs_variance, decreasing=TRUE)[1:arguments$number_of_most_variable_CpGs_to_include]
-
-  # Sorting the betas dataframe and getting only the number of CpGs selected
-  betas <- betas[sorting_vec,]
-}
-
-
-# ===================================================
-# DIVIDING DATASET IN FUNCTION OF THE INFINIUM METHOD
-# ===================================================
-
-if (arguments$split_based_on_design) {
-
-  designs <- readRDS(arguments$path_to_design_list)
-
-
-  betas_I <- betas[rownames(betas) %in% designs[["I"]],]
-  betas_II <- betas[rownames(betas) %in% designs[["II"]],]
-
-}
-
 # ========================================
 # CREATING TRAINING AND VALIDATION SUBSETS
 # ========================================
 
-#If the user has selected to filter sexual chromosomes the following code will be run
+#If the user has selected to split the data in training and validation the following code will be run
 if (arguments$split_in_training_and_validation) {
 
-  if (!arguments$split_based_on_design) {
-    cat("\n\nCreating training and validation datasets...\n")
+  cat("\n\nCreating training and validation datasets...\n")
 
-    #Setting the seed for spliting the data
-    set.seed(1)
+  #Setting the seed for spliting the data
+  set.seed(1)
 
-    #Splitting the data
-    split_ratio <- 1 - arguments$percentage_to_validation/100
-    train_samples <- sample.split(colnames(betas), SplitRatio=split_ratio)
+  #Splitting the data
+  split_ratio <- 1 - arguments$percentage_to_validation/100
+  train_samples <- sample.split(colnames(betas), SplitRatio=split_ratio)
 
-    #Creating training and validation datasets for the beta values
-    unadj_training <- betas[,train_samples]
-    unadj_validation <- betas[,!train_samples]
+  #Creating training and validation datasets for the beta values
+  unadj_training <- betas[,train_samples]
+  unadj_validation <- betas[,!train_samples]
 
-    #Creating training and validation datasets for the purity_vector
-    purity_training <- purities[train_samples]
-    purity_validation <- purities[!train_samples]
-    
-  } else {
+  #Creating training and validation datasets for the purity_vector
+  purity_training <- purities[train_samples]
+  purity_validation <- purities[!train_samples]
 
-    cat("\n\nCreating training and validation datasets for each of the beta estimation method...\n")
+  
+  } 
 
-    #Setting the seed for spliting the data
-    set.seed(1)
+# =================================================
+# GETTING MOST VARIABLE CpGs BASED ON TRAINING DATA
+# =================================================
 
-    #Splitting the data
-    split_ratio <- 1 - arguments$percentage_to_validation/100
-    train_samples <- sample.split(colnames(betas_I), SplitRatio=split_ratio)
-    
+# If the user has selected to get only the most variable CpGs the following code will be run
+if(arguments$get_only_CpGs_with_highest_variance) {
 
-    #Creating training and validation datasets for the beta values
-    unadj_training_I <- betas_I[,train_samples]
-    unadj_training_II <- betas_II[,train_samples]
-    unadj_validation_I <- betas_I[,!train_samples]
-    unadj_validation_II <- betas_II[,!train_samples]
+  cat("\n\nGetting CpGs with highest variance...\n")
 
-    #Creating training and validation datasets for the purity_vector
-    purity_training <- purities[train_samples]
-    purity_validation <- purities[!train_samples]
+  # Detemine the variance of all the rows (CpGs)
+  cpgs_variance <- apply(
+                        unadj_training,
+                        MARGIN=1,
+                        FUN=var
+                   )
+  
+  # Create a vector to sort the rows. Get only the number of rows containing the CpGs that want to be included
+  sorting_vec <- order(cpgs_variance, decreasing=TRUE)[1:arguments$number_of_most_variable_CpGs_to_include]
 
-  }
+  # Sorting the betas dataframe and getting only the number of CpGs selected
+  unadj_training <- unadj_training[sorting_vec,]
+  unadj_validation <- unadj_validation[sorting_vec,]
 }
 
 # =======================
@@ -361,8 +301,6 @@ cat("\n\nSaving the data...\n")
 #Run if the user has selected to split the data in training and validation subsets
 if (arguments$split_in_training_and_validation) {
 
-   if (!arguments$split_based_on_design) {  
-
     #Saving beta values
     saveRDS(unadj_training, paste(arguments$output_directory, "/betas_training.RData", sep=""))
     saveRDS(unadj_validation, paste(arguments$output_directory, "/betas_validation.RData", sep=""))
@@ -371,41 +309,14 @@ if (arguments$split_in_training_and_validation) {
     saveRDS(purity_training, paste(arguments$output_directory, "/purity_training.RData", sep=""))
     saveRDS(purity_validation, paste(arguments$output_directory, "/purity_validation.RData", sep=""))
 
-   } else {
-
-    #Saving beta values
-    saveRDS(unadj_training_I, paste(arguments$output_directory, "/betas_training_I.RData", sep=""))
-    saveRDS(unadj_training_II, paste(arguments$output_directory, "/betas_training_II.RData", sep=""))
-    saveRDS(unadj_validation_I, paste(arguments$output_directory, "/betas_validation_I.RData", sep=""))
-    saveRDS(unadj_validation_II, paste(arguments$output_directory, "/betas_validation_II.RData", sep=""))
-
-    #Saving purity value
-    saveRDS(purity_training, paste(arguments$output_directory, "/purity_training.RData", sep=""))
-    saveRDS(purity_validation, paste(arguments$output_directory, "/purity_validation.RData", sep=""))
-
-   }
-
 #Run if the user has not selected to split the data in training and validation subsets
 } else {
 
-  if (!arguments$split_based_on_design) {  
-
     #Saving beta values
-    saveRDS(arguments$betas, paste(arguments$output_directory, "/betas.RData", sep=""))
+    saveRDS(betas, paste(arguments$output_directory, "/betas.RData", sep=""))
     
     #Saving purity values
-    saveRDS(arguments$purity, paste(arguments$output_directory, "/purity.RData", sep=""))
-
-  } else {
-
-    #Saving beta values
-    saveRDS(arguments$betas_I, paste(arguments$output_directory, "/betas_I.RData", sep=""))
-    saveRDS(arguments$betas_I, paste(arguments$output_directory, "/betas_II.RData", sep=""))
-    
-    #Saving purity values
-    saveRDS(arguments$purity, paste(arguments$output_directory, "/purity.RData", sep=""))
-
-  }
+    saveRDS(purities, paste(arguments$output_directory, "/purity.RData", sep=""))
 
 }
 
