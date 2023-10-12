ac47c5fe9b983e2eaf8f7a31e4aecc3011636902
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-10-12 11:33:52 +0200

    Debugging

--- scripts/final_beta_correction/final_beta_correction_without_refitting.r
+++ scripts/final_beta_correction/final_beta_correction_without_refitting.r
@@ -30,6 +30,14 @@ argument_list <- list(
               help="Path to the file with the beta values to be corrected whose sample purity has been estimated. The file must be an R object containing a dataframe with the CpGs as rows and samples as columns.",
               metavar = "[file path]"),
 
+  make_option(c("-F", "--only_certain_CpGs"), type="logical", default=FALSE,   
+              help="This argument should be set TRUE if a list with the CpGs to correct wants to be provided.",
+              metavar = "[TRUE/FALSE]"),
+
+  make_option(c("-f", "--CpGs_to_correct_vec"), type="character",  
+              help="The path of the Robject containing a vector with the CpGs to correct should be entered here.",
+              metavar = "[file path]"),
+
   make_option(c("-o", "--output"), type="character", default="./",
               help="The path to the location where the output files will be saved must be entered here. The output is an R object. Default [%default]",
               metavar = "[file path]"),
@@ -56,21 +64,18 @@ my_intercepts <- readRDS(list.files(arguments$ref_regressions, pattern="*reg.int
 # Loading betas to correct
 to_correct_betas <- readRDS(arguments$betas_to_correct)
 
-#Adapt name of betas
-colnames(to_correct_betas) <- lapply(colnames(betas), 
-                         function (name) {strsplit(name, "-01")[[1]][1]})
 
 # Loading predicted 1 - purities
 predicted_1mPurities <- read.table(arguments$est_purity, 
-                                header=TRUE,
                                 sep="\t")
 
+
 # Removing samples with more than one estimates (if any)
-predicted_1mPurities <- predicted_1mPurities[which(predicted_purities[,"num_of_est"]==1),]
+predicted_1mPurities <- predicted_1mPurities[which(predicted_1mPurities[,2]==1),]
 
 # Transforming the predicted_1mPurities dataframe into a vector
-predicted_1mPurities_vec <- predicted_1mPurities[,"estimate"]
-names(predicted_1mPurities_vec) <- predicted_1mPurities[,"sample"]
+predicted_1mPurities_vec <- predicted_1mPurities[,3]
+names(predicted_1mPurities_vec) <- predicted_1mPurities[,1]
 
 
 
@@ -80,11 +85,22 @@ names(predicted_1mPurities_vec) <- predicted_1mPurities[,"sample"]
 
 cat("\nChecking cpgs...\n\n")
 
-if (length(!(rownames(to_correct_betas) %in% rownames(my_slopes))) != 0) {
+# Use only the specified CpGs if that option has been selected
+if (arguments$only_certain_CpGs) {
+
+  #Getting the vector
+  vec_of_cpgs <- readRDS(arguments$CpGs_to_correct_vec)
+
+  #Keeping only CpGs of interest
+  to_correct_betas <- to_correct_betas[vec_of_cpgs,]
+
+}
+
+if (sum(!(rownames(to_correct_betas) %in% rownames(my_slopes))) != 0) {
 
     # Printing warning message
-    cat("\n",  length(!(rownames(to_correct_betas) %in% rownames(my_slopes))), "CpGs are not included into the refernce regressions, so they can not be corrected.\n\n")
-
+    cat("\n",  sum(!(rownames(to_correct_betas) %in% rownames(my_slopes))), "CpG(s) is/are not included into the refernce cohort, so it/they can not be corrected.\n\n")
+    
     # Filtering not included CpGs
     to_correct_betas <- to_correct_betas[rownames(to_correct_betas) %in% rownames(my_slopes),]
 }
@@ -102,19 +118,24 @@ my_intercepts <- my_intercepts[rownames(my_intercepts) %in% rownames(to_correct_
 #Generating function to identify the refernce regression to which each CpG of each sample belongs
 identify_regression <- function(beta, estimated_1mPurity, vec_slopes, vec_intercepts) {
 
+
     # Generating a vector of distances
-    vec_of_dis <- sapply(c(1:length(vec_slopes)), 
+    vec_of_dis <- unname(sapply(c(1:length(vec_slopes)), 
                          FUN = function(pop) {
 
                                     #Ignoring NA values that appear when less than 3 populations are detected
                                     #in the refernce rrgressions
                                     if (!is.na(vec_slopes[pop])) {
 
-                                        vec_slopes[pop] * estimated_1mPurity + vec_intercepts[pop] - beta
+                                        beta - vec_slopes[pop] * estimated_1mPurity + vec_intercepts[pop]
+
+                                    } else {
+
+                                        NA
 
                                     }
                                 }
-                            )
+                            ))
 
     #Determining the population (vector index) with the lowest absolute distance. If the distances are equal the first
     #population with be chosen by default
@@ -122,9 +143,9 @@ identify_regression <- function(beta, estimated_1mPurity, vec_slopes, vec_interc
 
     #Generating and returning output named vector
     return(
-        c("Slope"=vec_slopes[pop],
-          "Itercepts"=vec_intercepts[pop],
-          "Distance"=vec_of_dis[pop])
+        c("Slope"=vec_slopes[pop_identified],
+          "Intercept"=vec_intercepts[pop_identified],
+          "Distance"=vec_of_dis[pop_identified])
         )
     }
 
@@ -132,6 +153,7 @@ identify_regression <- function(beta, estimated_1mPurity, vec_slopes, vec_interc
 #Generate function to correct betas based on the identified regression. The parametres specified must be 
 #from the beta VS 1-P regressions.
 correcting_betas <- function(slope, intercept, distance, to_correct) {
+
 
     if (to_correct=="Tumor") {
         #The tumor beta value will be obtained using the intercept and the calculated distance.
@@ -144,7 +166,8 @@ correcting_betas <- function(slope, intercept, distance, to_correct) {
 
         return(tum_beta)
 
-    } else if (to_correct=="Micoenvironment") {
+
+    } else if (to_correct=="Microenvironment") {
         #The microenvironment beta value will be obtained using the intercept and slope when 1-P=1 and the calculated distance.
         #The minimum possible value will allways be kept below or equal to 1
         env_beta <- if (intercept + slope + distance >= 0) {
@@ -162,20 +185,19 @@ correcting_betas <- function(slope, intercept, distance, to_correct) {
  corrected_tumor <- data.frame()
  corrected_microenvironment <- data.frame()
 
-
-
  for (cpg in rownames(to_correct_betas)) {
 
-    corrected_tumor[cpg,rownames(to_correct_betas)] <- sapply(names(predicted_1mPurities), 
+    corrected_tumor[cpg,colnames(to_correct_betas)] <- sapply(names(predicted_1mPurities_vec), 
 
         function(sample) {
 
         identified_reg <- identify_regression(
             beta=to_correct_betas[cpg, sample],
-            estimated_1mPurity=predicted_1mPurities[sample],
+            estimated_1mPurity=predicted_1mPurities_vec[sample],
             vec_slopes=my_slopes[cpg,],
             vec_intercepts=my_intercepts[cpg,]
             )
+
 
         corrected_tum <- correcting_betas(
             slope=identified_reg["Slope"],
@@ -184,23 +206,25 @@ correcting_betas <- function(slope, intercept, distance, to_correct) {
             to_correct="Tumor"
             )
 
+
         return(corrected_tum)
 
         }
     )
 
-    corrected_microenvironment[cpg,rownames(to_correct_betas)] <- sapply(names(predicted_1mPurities), 
+    corrected_microenvironment[cpg,colnames(to_correct_betas)] <- sapply(names(predicted_1mPurities_vec), 
 
         function(sample) {
 
         identified_reg <- identify_regression(
             beta=to_correct_betas[cpg, sample],
-            estimated_1mPurity=predicted_1mPurities[sample],
+            estimated_1mPurity=predicted_1mPurities_vec[sample],
             vec_slopes=my_slopes[cpg,],
             vec_intercepts=my_intercepts[cpg,]
             )
 
-        corrected_env <- env_beta(
+
+        corrected_env <- correcting_betas(
             slope=identified_reg["Slope"],
             intercept=identified_reg["Intercept"],
             distance=identified_reg["Distance"],
@@ -220,15 +244,19 @@ correcting_betas <- function(slope, intercept, distance, to_correct) {
 # =======================
 
 # Generating RObject files
-saveRDS(corrected_tumor, file=paste(arguments$output_location, arguments$output_filename, ".tumor.samples_to_correct.RData", sep=""))
-saveRDS(corrected_microenvironment, file=paste(arguments$output_location, arguments$output_filename, ".microenvironment.samples_to_correct.RData", sep=""))
+saveRDS(corrected_tumor, file=paste(arguments$output, arguments$output_name, ".tumor.samples_to_correct.RData", sep=""))
+saveRDS(corrected_microenvironment, file=paste(arguments$output, arguments$output_name, ".microenvironment.samples_to_correct.RData", sep=""))
 
 # Generating tsv files
 write.table(corrected_tumor, 
-            file=paste(arguments$output_location, arguments$output_filename, ".tumor.samples_to_correct.tsv", sep=""),
+            file=paste(arguments$output, arguments$output_name, ".tumor.samples_to_correct.tsv", sep=""),
             col.names=NA, 
             sep="\t")
 write.table(corrected_microenvironment, 
-            file=paste(arguments$output_location, arguments$output_filename, ".microenvironment.samples_to_correct.tsv", sep=""),
+            file=paste(arguments$output, arguments$output_name, ".microenvironment.samples_to_correct.tsv", sep=""),
             col.names=NA, 
             sep="\t")
+
+#cat("\n=================\n")
+#cat ("PROCESS FINISHED")
+#cat("\n=================\n")
