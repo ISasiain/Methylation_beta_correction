a47d749fe7ff7737261cc22f23f22d6f290b25d7
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-10-30 12:05:36 +0100

    Adding short code documentation

--- scripts/get_data_to_analyse/get_most_variable_cpgs.r
+++ scripts/get_data_to_analyse/get_most_variable_cpgs.r
@@ -1,3 +1,51 @@
+#!/usr/bin/Rscript
+
+# Script Name: get_most_variable_cpgs.r
+
+# Description:
+# This script allows users to select the most variable CpGs from a reference dataset based on variance or by specifying a number. The selected CpGs are extracted from the dataset to be analyzed.
+
+# Required R Packages:
+# - optparse: For parsing command-line arguments.
+
+# Configuration of Command Line Arguments:
+# - The script uses the 'optparse' package to configure and parse command-line arguments.
+# - The available options include:
+#   - "--reference_dataset": Path to the RObject containing the reference data (betas of each CpG).
+#   - "--data_to_analyse": Path to the RObject containing the data to analyze (betas of each CpG).
+#   - "--filter_by_number": Set to TRUE to select a specific number of the most variable CpGs.
+#   - "--number_of_CpGs": Number of most variable CpGs to keep.
+#   - "--filter_by_variance": Set to TRUE to select CpGs over a specific variance threshold.
+#   - "--variance": Variance threshold for filtering CpGs.
+#   - "--include_CpG_list": Set to TRUE to generate a file containing the included CpG names.
+#   - "--prefix": Prefix for the output file names.
+
+# Loading Required R Packages:
+# - The script loads the 'optparse' package for command-line argument parsing.
+
+# Parsing Command Line Arguments:
+# - The 'optparse' package is used to parse the command-line arguments, including file paths and options for filtering CpGs based on variance or a specific number.
+
+# Loading Data:
+# - The script reads the reference and data to analyze from RObject files using the 'readRDS' function.
+
+# Getting Most Variable CpGs:
+# - The script calculates the variance for all CpGs in the reference dataset.
+# - If filtering by a specific number of CpGs is selected, it sorts the CpGs based on variance and selects the top 'number_of_CpGs'.
+# - If filtering by variance threshold is selected, it chooses CpGs with variances greater than or equal to the specified 'variance' threshold.
+
+# Generating Output:
+# - The selected CpGs are used to extract relevant data from the dataset to be analyzed.
+# - The extracted data is saved as an RObject file with a user-defined prefix and file extension "_most_variable_CpGs.RData."
+# - If the 'include_CpG_list' option is set to TRUE, a separate RObject file is created with the names of the included CpGs.
+
+# Example Usage:
+# - Users can run the script from the command line with various options to select the most variable CpGs based on their preferences.
+
+# Author: Iñaki Sasiain Casado
+# Affiliation: Johan Staaf lab @ Lund University / Oncology & Pathology
+
+
 # =============================
 # LOADING THE REQUIRED PACKAGES
 # =============================
