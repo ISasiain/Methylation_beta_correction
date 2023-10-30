69ca314120336c2ddd89e8af219d2247e360fd23
Iñaki Sasiain <inakisasiainca@gmail.com>
2023-10-30 13:12:32 +0100

    Update script. Add documentation

--- scripts/get_data_to_analyse/split_based_on_purity.r
+++ scripts/get_data_to_analyse/split_based_on_purity.r
@@ -1,3 +1,28 @@
+# Script Name: split_based_on_purity.r
+
+# Description:
+# This script is designed to split beta values and purity data into eight intervals based on specified purity intervals. It loads beta values and purity data, then creates separate data subsets for each purity interval.
+# Important: The purity_validation.r and betas_validation.r scripts must be located a directory before the workin directory.
+
+# Loading Data:
+# - The script reads beta values from an RData file ("betas_validation.RData") and purity data from another RData file ("purity_validation.RData").
+
+# Creating Purity Intervals:
+# - The script defines a list, "list_to_split," containing eight purity intervals with specified purity range values.
+# - Each interval is named from "from_1_to_0.7" to "from_0.2_to_0."
+
+# Iterating through Purity Intervals:
+# - The script iterates through the "list_to_split," creating data subsets for each purity interval.
+# - For each interval, it filters and assigns the beta values and purity values corresponding to that interval.
+# - The filtered beta values and purity values are saved as separate RData files with specific names, such as "betas_from_1_to_0.7.RData" and "pur_from_1_to_0.7.RData."
+
+# Example Usage:
+# - Users can run the script to split beta values and purity data into specific purity intervals as defined in the "list_to_split."
+
+# Author: Iñaki Sasiain Casado
+# Affiliation: Johan Staaf lab
+
+
 #Loading the data
 betas <- readRDS("../betas_validation.RData");
 purities <- readRDS("../purity_validation.RData");
